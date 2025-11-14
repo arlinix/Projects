c9c9840d1e15094de36d8e2169cfa8fec3a72f85
Arinix <amanrajpoot94119@gmail.com>
2025-11-14 14:42:01 +0530

    Fix the constructor of com.myshop.model.DefaultProduct.java

--- Online-Shop/src/main/java/com/myshop/model/DefaultProduct.java
+++ Online-Shop/src/main/java/com/myshop/model/DefaultProduct.java
@@ -6,9 +6,12 @@ public class DefaultProduct implements Product {
     private String categoryName;
     private double price;
 
-    public DefaultProduct(int id, String productName) {
+    public DefaultProduct(int id, String productName,String categoryName,  double price) {
         this.id = id;
         this.productName = productName;
+        this.categoryName = categoryName;
+        this.price = price;
+
     }
 
     @Override
