a0f3fde14ed0ca00a23d170f168ca69beac37ced
Arinix <amanrajpoot94119@gmail.com>
2025-11-17 15:42:15 +0530

    Add DBConnectionFactory to com.myshop/db

---
+++ Online-Shop-WebUI/src/main/java/com/myshop/db/DbConnectionFactory.java
@@ -0,0 +1,31 @@
+package com.myshop.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnectionFactory {
+
+    // Update these credentials to match your environment
+    private static final String URL = "jdbc:mysql://localhost:3306/myshop?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "myshop_admin";
+    private static final String PASSWORD = "123456";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("MySQL Driver loaded.");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Unable to load MySQL driver", e);
+        }
+    }
+
+    private DbConnectionFactory() {}
+
+    /**
+     * New Connection (caller is responsible for closing it).
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
